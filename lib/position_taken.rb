976fb5e812035021aa8738e4d885d12bfb4c6210
Lizzie Chan <lchan217@gmail.com>
2019-01-28 16:40:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
@@ -10,4 +11,19 @@ def position_taken?(board, index)
   else  
     false
   end
+end
+=======
+# code your #position_taken? method here!
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    "true"
+  elsif board[index] == " "  
+    "false"
+  elsif board[index] == ""
+    "false"
+  else  
+    "false"
+  end
+>>>>>>> 9df18953f2be0b2bde2d3adac1101c610f1b5da5
 end
